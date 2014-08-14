aa73fb82151da0d1e0d8a890ac23642c2eb10973
biankai <biankai@kingsoft.com>
2014-08-14 15:02:32 +0800

    update the newest 0814 ~

--- js/lyx.js
+++ js/lyx.js
@@ -9,4 +9,29 @@ $(function () {
 		$(".index-wrap").show();
 	});
 
+	// slide
+	var i = $('.swipe-wrap div').length;
+	var $li = '';
+	for(var j=0; j<i; j++ ){
+		$li += '<li></li>';
+	}
+	$('#swipe-page').append($li);
+	$('#swipe-page').find('li').eq(0).addClass('on');
+	var elem = document.getElementById('mySwipe');
+	window.mySwipe = Swipe(elem, {
+	  startSlide: 0,
+	  auto: 3000,
+	  continuous: true,
+	  disableScroll: true,
+	  stopPropagation: true,
+	  callback: function(pos) {
+	  	var bullets = document.getElementById('swipe-page').getElementsByTagName('li');
+	  	var i = bullets.length;
+        while (i--) {
+          bullets[i].className = ' ';
+        }
+        bullets[pos].className = 'on';
+	    },
+	  transitionEnd: function(index, element) {}
+	});
 });
